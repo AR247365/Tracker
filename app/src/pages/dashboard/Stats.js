cabd6ff825b3a45684398bd06bccd56f891e99ac
AR247365 <lonewolf22081998@gmail.com>
2023-09-16 01:05:19 +0530

    search

--- app/src/pages/dashboard/Stats.js
+++ app/src/pages/dashboard/Stats.js
@@ -10,7 +10,7 @@ const Stats = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(showStats())
-  }, [])
+  }, [dispatch])
   if (isLoading) {
     return <Loading center />
   }
