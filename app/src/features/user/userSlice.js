4c1ca731c5f2145334a7b89cd5917eb796ca6811
AR247365 <lonewolf22081998@gmail.com>
2023-09-18 17:09:07 +0530

    Refactor

--- app/src/features/user/userSlice.js
+++ app/src/features/user/userSlice.js
@@ -5,7 +5,12 @@ import {
   getUserFromLocalStorage,
   removeUserFromLocalStorage,
 } from "../../utils/localStorage"
-import { loginUserThunk, registerUserThunk, updateUserThunk } from "./userThunk"
+import {
+  loginUserThunk,
+  registerUserThunk,
+  updateUserThunk,
+  clearStoreThunk,
+} from "./userThunk"
 
 const initialState = {
   isLoading: false,
@@ -32,6 +37,7 @@ export const updateUser = createAsyncThunk(
     return updateUserThunk("/auth/updateUser", user, thunkAPI)
   }
 )
+export const clearStore = createAsyncThunk("user/clearStore", clearStoreThunk)
 
 const userSlice = createSlice({
   name: "user",
@@ -49,50 +55,55 @@ const userSlice = createSlice({
       }
     },
   },
-  extraReducers: {
-    [registerUser.pending]: (state) => {
-      state.isLoading = true
-    },
-    [registerUser.fulfilled]: (state, { payload }) => {
-      const { user } = payload
-      state.isLoading = false
-      state.user = user
-      addUserToLocalStorage(user)
-      toast.success(`Hello ${user.name}`)
-    },
-    [registerUser.rejected]: (state, { payload }) => {
-      state.isLoading = false
-      toast.error(payload)
-    },
-    [loginUser.pending]: (state) => {
-      state.isLoading = true
-    },
-    [loginUser.fulfilled]: (state, { payload }) => {
-      const { user } = payload
-      state.isLoading = false
-      state.user = user
-      addUserToLocalStorage(user)
-      toast.success(`Welcome back ${user.name}`)
-    },
-    [loginUser.rejected]: (state, { payload }) => {
-      state.isLoading = false
-      toast.error(payload)
-    },
-    [updateUser.pending]: (state) => {
-      state.isLoading = true
-    },
-    [updateUser.fulfilled]: (state, { payload }) => {
-      const { user } = payload
-      state.isLoading = false
-      state.user = user
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        const { user } = payload
+        state.isLoading = false
+        state.user = user
+        addUserToLocalStorage(user)
+        toast.success(`Hello There ${user.name}`)
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false
+        toast.error(payload)
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        const { user } = payload
+        state.isLoading = false
+        state.user = user
+        addUserToLocalStorage(user)
 
-      addUserToLocalStorage(user)
-      toast.success("User Updated")
-    },
-    [updateUser.rejected]: (state, { payload }) => {
-      state.isLoading = false
-      toast.error(payload)
-    },
+        toast.success(`Welcome Back ${user.name}`)
+      })
+      .addCase(loginUser.rejected, (state, { payload }) => {
+        state.isLoading = false
+        toast.error(payload)
+      })
+      .addCase(updateUser.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(updateUser.fulfilled, (state, { payload }) => {
+        const { user } = payload
+        state.isLoading = false
+        state.user = user
+        addUserToLocalStorage(user)
+
+        toast.success(`User Updated!`)
+      })
+      .addCase(updateUser.rejected, (state, { payload }) => {
+        state.isLoading = false
+        toast.error(payload)
+      })
+      .addCase(clearStore.rejected, () => {
+        toast.error("There was an error..")
+      })
   },
 })
 
